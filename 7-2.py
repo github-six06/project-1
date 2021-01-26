e735962aa1f057877e341f5b5d7838643f25f42c
曾云丹 <77480043+swine619@users.noreply.github.com>
2021-01-26 15:17:04 +0800

    Delete 7-2.py

--- 7-2.py
+++
@@ -1,6 +0,0 @@
-number=input("please tell me how many persons will eat here?")
-number=int(number)
-if number>8:
-	print("\nSorry,there is not any free table !")
-else:
-	print("\nOK! There are ehough seats leave,lte me serve you!")
