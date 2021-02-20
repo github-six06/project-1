b27aa776f2eda4d992337bb7ff4bf1832315d6a0
KRISJ-L <77475062+KRISJ-L@users.noreply.github.com>
2021-02-20 11:41:57 +0800

    Update 15-9.py

--- 15-9.py
+++ 15-9.py
@@ -1,23 +1,29 @@
 
-#15-9
+from die import Die
 import pygal
-from chapter15.die import Die
-
-die1=Die()
-die2=Die()
-
-results=[die1.roll()*die2.roll() for roll_num in range(50000)]
-
-#分析结果
-max_result=die1.num_sides*die2.num_sides
-frequences = [results.count(value) for value in range(1,max_result+1)]
-
-#对结果进行可视化
+def num2char(x):
+	return str(x)
+die1 = Die()
+die2 = Die()
+results = []
+ 
+for roll_num in range(1000):
+	result = die1.roll() * die2.roll()
+	results.append(result)
+# print(results)
+frequencies = []
+max_result = die1.num_sides * die2.num_sides
+for value in range(1,max_result+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+ 
 hist = pygal.Bar()
-hist.title="Results of rolling three D6 50000 times."
-hist.x_labels=list(range(1,37))
-hist.x_title="Result"
-hist._y_title="Frequence of Result"
+hist.title = "Result of rolling me D6 1000 times."
+hist.x_labels = list(map(num2char,list(range(1,max_result+1))))
+print(hist.x_labels)
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+ 
+hist.add('D6*D6',frequencies)
+hist.render_to_file('dice1_visual.svg')
 
-hist.add('D6*D6',frequences)
-hist.render_to_file('multip_visual.svg')
