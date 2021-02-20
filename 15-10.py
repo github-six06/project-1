3476c38db5de93a67054c34569a633abe0a7a2ea
KRISJ-L <77475062+KRISJ-L@users.noreply.github.com>
2021-02-20 11:42:58 +0800

    Update 15-10.py

--- 15-10.py
+++ 15-10.py
@@ -1,25 +1,23 @@
+
 import matplotlib.pyplot as plt
 from die import Die
-die_1 = Die()
-die_2 = Die()
-
-#创立2个D6
+ 
+die1 = Die()
+die2 = Die()
 results = []
-
+ 
 for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
-
-#分析分别出现几次
+	result = die1.roll() + die2.roll()
+	results.append(result)
+# print(results)
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
+max_result = die1.num_sides + die2.num_sides
 for value in range(1,max_result+1):
-    frequecy = results.count(value)
-    frequencies.append(frequecy)
-
-x_values = list(range(1,max_result+1))
-y_values = frequencies
-# 设置绘图窗口的尺寸
-plt.figure(figsize=(20, 12))
-plt.scatter(x_values,y_values, s=15)
+	frequency = results.count(value)
+	frequencies.append(frequency)
+print(frequencies)
+plt.plot(list(range(1,max_result+1)),frequencies,linewidth = 2)
+plt.title("Result of rolling me D6 1000 times.")
+plt.xlabel ("Result",fontsize = 14)
+plt.ylabel("Square of D6 1000 times")
 plt.show()
