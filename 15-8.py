1d8559b81ead47db2ffcb9d2a8ab5093ddd7fc1b
KRISJ-L <77475062+KRISJ-L@users.noreply.github.com>
2021-02-20 11:40:42 +0800

    Update 15-8.py

--- 15-8.py
+++ 15-8.py
@@ -1,36 +1,25 @@
-#15-8
-die.py
-from random import randint
-
-class Die():
-    """表示一个骰子的类"""
-    def __init__(self, num_sides=6):
-        """骰子默认为6面"""
-        self.num_sides = num_sides
-
-    def roll(self):
-        """返回一个位于1和骰子面数之间的随机值"""
-        return randint(1, self.num_sides)
-three dice.py
+from die import Die
 import pygal
-from chapter15.die import Die
-
-die1=Die()
-die2=Die()
-die3=Die()
-
-results=[die1.roll()+die2.roll()+die3.roll() for roll_num in range(50000)]
-
-#分析结果
-max_result=die1.num_sides+die2.num_sides+die3.num_sides
-frequences = [results.count(value) for value in range(3,max_result+1)]
-
-#对结果进行可视化
+ 
+die1 = Die()
+die2 = Die()
+die3 = Die()
+results = []
+ 
+for roll_num in range(1000):
+	result = die1.roll() + die2.roll() +  die3.roll()
+	results.append(result)
+frequencies = []
+max_result = die1.num_sides + die2.num_sides + die3.num_sides
+for value in range(2,max_result+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+ 
 hist = pygal.Bar()
-hist.title="Results of rolling three D6 50000 times."
-hist.x_labels=list(range(3,19))
-hist.x_title="Result"
-hist._y_title="Frequence of Result"
-
-hist.add('D6+D6+D6',frequences)
-hist.render_to_file('three_visual.svg')
+hist.title = "Result of rolling me D6 1000 times."
+hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+ 
+hist.add('D6+D6+D6',frequencies)
+hist.render_to_file('dice1_visual.svg')
