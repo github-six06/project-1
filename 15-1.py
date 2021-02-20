20fa1b7b83c1fdc961b8c5a01fd2de99937e7660
KRISJ-L <77475062+KRISJ-L@users.noreply.github.com>
2021-02-20 11:33:20 +0800

    Update 15-1.py

--- 15-1.py
+++ 15-1.py
@@ -1,14 +1,23 @@
-import matplotlib.pyplot as plt
 
-x_vaules = list(range(1,5000))
-y_values = [x*x*x for x in x_vaules]
-plt.scatter(x_vaules,y_values,edgecolors='none',s = 40)
-#设置图表标题并给坐标轴加上标签
-plt.title("Square Numbers",fontsize = 24)
+import matplotlib.pyplot as plt
+ x_values  = [1,2,3,4,5]
+y_values = [1,8,27,64,125]
+plt.title("Li Fang ")
+plt.plot(x_values,y_values,linewidth = 5)
 plt.xlabel("Value",fontsize = 14)
 plt.ylabel("Square of Value",fontsize = 14)
-#设置刻度数值的大小
-plt.tick_params(axis = 'both',labelsize = 14)
+plt.tick_params(axis = 'both' , labelsize = 14)
+plt.show()
+
+
+import matplotlib.pyplot as plt
+x_values  = list(range(1,50001))
+y_values = [x**3 for x in x_values]
+plt.title("Li Fang ")
+plt.scatter(x_values,y_values,linewidth = 5,edgecolor = 'none')
+plt.xlabel("Value",fontsize = 14)
+plt.ylabel("Square of Value",fontsize = 14)
+plt.tick_params(axis = 'both' , labelsize = 14)
 plt.show()
 
 
