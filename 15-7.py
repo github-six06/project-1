c3f5329d8b229d89c3fccac2ea8eb6966237ab5e
KRISJ-L <77475062+KRISJ-L@users.noreply.github.com>
2021-02-20 11:39:06 +0800

    Update 15-7.py

--- 15-7.py
+++ 15-7.py
@@ -1,29 +1,29 @@
-import pygal
+from random import randint
+class Die(object):
+	"""docstring for Die"""
+	def __init__(self, num_sides = 8):
+		self.num_sides = num_sides
+ 
+	def roll(self):
+		return randint(1,self.num_sides)
 from die import Die
-die_1 = Die(8)
-die_2 = Die(8)
-#创立2个D6
+import pygal
+die1 = Die()
+die2 = Die()
 results = []
-
-for roll_num in range(10000000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
-
-#分析分别出现几次1-6
+for roll_num in range(1000):
+	result = die1.roll() + die2.roll()
+	results.append(result)
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
+max_result = die1.num_sides + die2.num_sides
 for value in range(2,max_result+1):
-    frequecy = results.count(value)
-    frequencies.append(frequecy)
-
-#对结果进行可视化
+	frequency = results.count(value)
+	frequencies.append(frequency)
 hist = pygal.Bar()
-
-hist.title = 'Results of rolling two D8 dice 10000 times.'
+hist.title = "Result of rolling me D8 1000 times."
 hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
-
+ 
 hist.add('D8+D8',frequencies)
-hist.render_to_file('15.7.svg')
-
+hist.render_to_file('dice1_visual.svg')
